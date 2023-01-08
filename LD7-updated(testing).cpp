60550d1a4d8f9a095f269b1c0100463bdf0366c2
Albert <121330869+Qrebla@users.noreply.github.com>
2023-01-08 17:27:40 +0800

    Update LD7-updated(testing).cpp

--- LD7-updated(testing).cpp
+++ LD7-updated(testing).cpp
@@ -4,7 +4,6 @@
 using namespace std;
 
 int Speed, start = 2, pause = 3, stop = 6;
-char alpha;
 /*
 1 : VK_SPACE
 2 : VK_RETURN
@@ -14,7 +13,7 @@ char alpha;
 6 : VK_CONTROL
 */
 
-bool GetKeyboardEvent(int a){
+int GetKeyboardEvent(int a){
 	
     switch (a) {
         case 1:
@@ -37,7 +36,18 @@ bool GetKeyboardEvent(int a){
             break;
         default:
             return  GetAsyncKeyState(a);
+            break;
     }
+}
+
+int GetLetter(int num) {
+	if(num != 7 && num != 8)
+		return num;
+	printf("Character:");
+	char alpha;
+	cin >> alpha;
+	alpha = toupper(alpha);
+	return (int)alpha;
 }
 
 void input(){
@@ -49,28 +59,13 @@ void input(){
     printf("\t1 : spacebar\n\t2 : enter\n\t3 : alt\n\t4 : esc\n\t5 : tab\n\t6 : control\n\t7 : A-Z\n\t8 : 0-9\n");
     printf("to start:" );
     scanf("%d", &start);
-    if(start==7||start==8){
-    	printf("Character:");
-    	cin>>alpha;
-    	alpha = toupper(alpha);
-    	start = alpha;
-    }
+    start = GetLetter(start);
     printf("\nto pause:" );
     scanf("%d", &pause);
-    if(pause==7||pause==8){
-    	printf("Character:");
-    	cin>>alpha;
-    	alpha = toupper(alpha);
-    	pause = alpha;
-    }
+    pause = GetLetter(pause);
     printf("\nto end:" );
     scanf("%d", &stop);
-    if(stop==7||stop==8){
-    	printf("Character:");
-    	cin>>alpha;
-    	alpha = toupper(alpha);
-    	stop = alpha;
-    }
+    stop = GetLetter(stop);
     Sleep(100);
     puts("* Settings are ready, you can start whenever you want :)");
     Sleep(1000);
@@ -81,16 +76,16 @@ void main_in_main(){
         while(1){
         	//if(GetKeyboardEvent(stop))  //Stop  
             //    return;
-            if(GetKeyboardEvent(start))//press space to start
-                while(1) {
-                    mouse_event(MOUSEEVENTF_LEFTDOWN|MOUSEEVENTF_LEFTUP,0,0,0,0);//Mouse click st current spot
-                    Sleep(Speed);//Click speed control
-                    if(GetKeyboardEvent(pause)) goto oto; //Pause 
-                    if(GetKeyboardEvent(stop)) { //Stop  
-                    	return;
-                    }
-                }
-            }
+            if(GetKeyboardEvent(start)){
+				while(1) {
+	                mouse_event(MOUSEEVENTF_LEFTDOWN|MOUSEEVENTF_LEFTUP,0,0,0,0);//Mouse click st current spot
+	                Sleep(Speed);//Click speed control
+	                if(GetKeyboardEvent(pause)) goto oto; //Pause 
+	                if(GetKeyboardEvent(stop)) return;
+	            }
+			}//press space to start
+                
+        }
     }
 }
 
@@ -98,6 +93,7 @@ int main() {
 	input();
     main_in_main();
 	puts("Thx 4 using!");
+	puts("Made by Albert(Qrebla) and 11400F");
 	Sleep(1000); 
 	
 	return 0;
